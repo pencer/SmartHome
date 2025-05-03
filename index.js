31ee2eaaa142d3861ad7390df74e40ee6c3b8e03
Yuki Kobayashi <kobayashi.yuki@gmail.com>
2025-05-04 00:30:11 +0900

    use redirect

--- index.js
+++ index.js
@@ -30,160 +30,160 @@ app.get('/test', function (req, res) {
   res.send('Test World!!!');
 });
 app.get('/remotectrl', function (req, res) {
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/weather', function (req, res) {
   execSync('/home/pi/bin/vieracontrol.php D1 ; sleep 7 ; /home/pi/bin/vieracontrol.php DATA ; sleep 2 ; /home/pi/bin/vieracontrol.php RED ; sleep 3 ; /home/pi/bin/vieracontrol.php ENTER');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/on', function (req, res) {
   exec('/home/pi/bin/wol_viera.sh');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/off', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php POWER');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/volup', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php VOLUP');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/voldown', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php VOLDOWN');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/ch_up', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php CH_UP');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/ch_down', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php CH_DOWN');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/d0', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php D0');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/d1', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php D1');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/d2', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php D2');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/d3', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php D3');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/d4', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php D4');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/d5', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php D5');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/d6', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php D6');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/d7', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php D7');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/d8', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php D8');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/d9', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php D9');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/blue', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php BLUE');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/red', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php RED');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/green', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php GREEN');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/yellow', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php YELLOW');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/enter', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php ENTER');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/return', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php RETURN');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/rec', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php REC');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/stop', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php STOP');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/pause', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php PAUSE');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/reclist', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php RECLIST');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/data', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php DATA');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/epg', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php EPG');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/viera/chg_input', function (req, res) {
   exec('/home/pi/bin/vieracontrol.php CHG_INPUT');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 // Music
 app.get('/music/radio/radio', function (req, res) {
   exec('USE_MPG123=1 BTDEVICE=" " /home/pi/work/radio/radio.sh');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/music/radio/radio0', function (req, res) {
   exec('USE_MPG123=1 BTDEVICE=" " /home/pi/work/radio/radio.sh 0');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/music/radio/radio1', function (req, res) {
   exec('USE_MPG123=1 BTDEVICE=" " /home/pi/work/radio/radio.sh 1');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/music/radio/radio2', function (req, res) {
   exec('USE_MPG123=1 BTDEVICE=" " /home/pi/work/radio/radio.sh 2');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/music/radio/radio3', function (req, res) {
   exec('USE_MPG123=1 BTDEVICE=" " /home/pi/work/radio/radio.sh 3');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/music/radio/radio4', function (req, res) {
   exec('USE_MPG123=1 BTDEVICE=" " /home/pi/work/radio/radio.sh 4');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/music/radio/radio5', function (req, res) {
   exec('USE_MPG123=1 BTDEVICE=" " /home/pi/work/radio/radio.sh 5');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/music/radio/radio6', function (req, res) {
   exec('USE_MPG123=1 BTDEVICE=" " /home/pi/work/radio/radio.sh 6');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/music/bluetoothctl/conn', function (req, res) {
   const result = execSync('/home/pi/work/radio/conn_btspeaker.sh 6C:5A:B5:70:F8:2A conn').toString();
@@ -196,76 +196,80 @@ app.get('/music/bluetoothctl/pair', function (req, res) {
 // Soundbar (remote)
 app.get('/remote/raspberrypi/soundbar/power', function (req, res) {
   exec('wget http://192.168.1.23:3001/soundbar/power -O /dev/null -o /dev/null');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/remote/raspberrypi/soundbar/volup', function (req, res) {
   exec('wget http://192.168.1.23:3001/soundbar/volup -O /dev/null -o /dev/null');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/remote/raspberrypi/soundbar/voldown', function (req, res) {
   exec('wget http://192.168.1.23:3001/soundbar/voldown -O /dev/null -o /dev/null');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/remote/raspberrypi/soundbar/bluetooth', function (req, res) {
   exec('wget http://192.168.1.23:3001/soundbar/bluetooth -O /dev/null -o /dev/null');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/remote/raspberrypi/soundbar/pairing', function (req, res) {
   exec('wget http://192.168.1.23:3001/soundbar/pairing -O /dev/null -o /dev/null');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/remote/raspberrypi/soundbar/sound', function (req, res) {
   exec('wget http://192.168.1.23:3001/soundbar/sound -O /dev/null -o /dev/null');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
+});
+app.get('/remote/raspberrypi/soundbar/input', function (req, res) {
+  exec('wget http://192.168.1.23:3001/soundbar/input -O /dev/null -o /dev/null');
+  res.redirect('/');
 });
 // Light
 app.get('/light/2fmain/lampcolor', function (req, res) {
   exec('/home/pi/bin/irc_send.sh light_2fmain_lampcolor');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/light/2fmain/neutralwhite', function (req, res) {
   exec('/home/pi/bin/irc_send.sh light_2fmain_neutralwhite');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/light/2fmain/next', function (req, res) {
   exec('/home/pi/bin/irc_send.sh light_2fmain_next');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/light/2fmain/small', function (req, res) {
   exec('/home/pi/bin/irc_send.sh light_2fmain_lampcolor && sleep 1 && /home/pi/bin/irc_send.sh light_2fmain_next');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/light/2fmain/off', function (req, res) {
   exec('/home/pi/bin/irc_send.sh light_2fmain_off');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/light/1fliving/0', function (req, res) {
   exec('/home/pi/bin/irc_send.sh light_living_0');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/light/1fliving/1', function (req, res) {
   exec('/home/pi/bin/irc_send.sh light_living_1');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/light/1fdining/all', function (req, res) {
   exec('/home/pi/bin/irc_send.sh light_dining_all');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/light/1fdining/dimmer', function (req, res) {
   exec('/home/pi/bin/irc_send.sh light_dining_dimmer');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/light/1fdining/small', function (req, res) {
   exec('/home/pi/bin/irc_send.sh light_dining_small');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/light/1fdining/off', function (req, res) {
   exec('/home/pi/bin/irc_send.sh light_dining_off');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/light/1fdining/next', function (req, res) {
   exec('/home/pi/bin/irc_send.sh light_dining_next');
-  res.render("index", {stat_msg: ""});
+  res.redirect('/');
 });
 app.get('/ac/1fac/stop', function (req, res) {
   exec('/home/pi/bin/ac1.sh stop');
